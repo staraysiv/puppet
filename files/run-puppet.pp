200add72b1b45f74d7a709822b61730302f5d459
staraysiv <41113570+staraysiv@users.noreply.github.com>
2018-07-26 19:26:10 +0530

    Update run-puppet.pp

--- files/run-puppet.pp
+++ files/run-puppet.pp
@@ -1,5 +1,6 @@
 # Set up regular Puppet runs
 file { '/usr/local/bin/run-puppet':
+  ensure => 'present',
   source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
   mode   => '0755',
 }
